d1bb9a55018fbc235cbde78f7cd45ffa11816ab3
kudzanai92 <keezentpty@gmail.com>
2023-02-16 18:42:57 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 
 {
-	fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1) success
+	fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
 }
