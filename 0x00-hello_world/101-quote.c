a50589816d79fba368fda9d0d54c96ea9932fa20
kudzanai92 <keezentpty@gmail.com>
2023-02-16 17:33:57 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unixstd.h>
+
+/**
+ * main - c program that prints a line to the standard error
+ * Return: 1 successed
+ */
+
+int main(void)
+
+{
+	fput(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1) success
+}
