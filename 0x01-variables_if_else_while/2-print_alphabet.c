d289db67418c8a32c8b4435fe04401a7caf1b966
kudzanai92 <keezentpty@gmail.com>
2023-02-18 07:26:55 -0800

    last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'a':; i <= 'z' : i++
+		putcher(i);
+	putcher('\n')
+		return (0);
+}
