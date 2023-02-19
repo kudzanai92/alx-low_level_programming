aecea264da4605419f7bd5ac25312f41176c56e9
kudzanai92 <keezentpty@gmail.com>
2023-02-18 18:19:01 -0800

    6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,19 +3,21 @@
 /**
  * main - main function
  *
- * Return Always 0
- *
+ * Return: Alwys 0
  */
 
 int main(void)
 
 {
-	char i;
+	int i;
 
 	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
 	putchar('\n');
 	return (0);
+
+
+
 
 
 
