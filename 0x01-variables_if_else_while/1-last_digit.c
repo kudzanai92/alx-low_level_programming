4d7e5a666879d71445a6d7f81ffa07564fad620b
kudzanai92 <keezentpty@gmail.com>
2023-02-19 12:53:20 -0800

    1-1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,13 +6,10 @@
 /**
  * main - main function
  *
- * Return: Always 0
+ * Return: always 0
  */
-
 int main(void)
-
 {
-
 	int n;
 	int m;
 
@@ -21,11 +18,11 @@ int main(void)
 	/* your code goes there */
 	m = n % 10;
 	if (m > 5)
-		printf("last digit of %d is %d and is greater than 5", n, m);
+		printf("Last digit of %d is %d and is greater than 5", n, m);
 	else if (m == 0)
-		printf("last digit of %d is %d and is 0", n, m);
+		printf("Last digit of %d is %d and is 0", n, m);
 	else if (m < 6 && m != 0)
-		printf("last digit of %d is %d and is less than 6 and not 0", n, m);
+		printf("Last digit of %d is %d and is less 6 and not 0", n, m);
 	printf("\n");
 
 	return (0);
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -25,8 +25,8 @@ int main(void)
 					putchar(k + '0');
 					if (i + j + k != 24)
 					{
-					putchar(',');
-					putchar(' ');
+						putchar(',');
+						putchar(' ');
 					}
 				}
 			}
