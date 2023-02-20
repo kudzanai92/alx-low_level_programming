52ac557b53ea925f270a5c057c15dd7ff7cea613
kudzanai92 <keezentpty@gmail.com>
2023-02-20 08:40:08 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - print and that piece of art is useful
  *
- * Return: Always 0 (success)
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	system("echo 'and that piece of art is useful\' \ - Dora Korpar, 2015-10-19'",);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
         return (1);
 }
