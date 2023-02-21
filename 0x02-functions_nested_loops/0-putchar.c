5f7462dd8b61ab413e125409ce9aec3f9510ca8a
kudzanai92 <keezentpty@gmail.com>
2023-02-20 23:58:24 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - Entry 
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char *sh = "Holberton";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+		
+	}
+	_putchar('\n');
+
+	return (0);
+}
