ba6c13e1f38f0e15e5f6e51239345d097334d1f8
kudzanai92 <keezentpty@gmail.com>
2023-02-21 15:43:57 -0800

    sign

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,8 +3,5 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);
-void print_to_98(int n);
-int add(int, int);
-int _isalpha(int c);
+int print_sign(int n);
 #endif
