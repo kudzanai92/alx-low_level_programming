9967eaa9db54043795af5f368f8ad3ab14f1efeb
kudzanai92 <keezentpty@gmail.com>
2023-02-21 09:59:14 -0800

    slower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - functiont to print the lower case
+ *
+ * c: parameter to be printed
+ *
+ * Return: 1 if there is a lower case
+ * and 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
