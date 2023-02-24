dfad98f3cb210f8fed5dfeafd333f963bb90ab6d
kudzanai92 <keezentpty@gmail.com>
2023-02-23 17:55:16 -0800

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,14 @@
  * Return: The number from 0 upto 9
  */
 
-void print_numbers(void);
+void print_numbers(void)
 
 {
 	int x;
 
 	for (x = 0; x <= 9; x++)
 	{
-	putchar(x + = '0');
+	_putchar(x + '0');
 	}
 	_putchar('\n');
 }
