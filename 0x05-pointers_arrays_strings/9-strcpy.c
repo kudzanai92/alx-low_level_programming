552a0c04a99ee6cd0b2ef46dcc1931f6046a59ab
kudzanai92 <keezentpty@gmail.com>
2023-02-27 16:53:29 -0800

    dust

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string pointed 
+ * @dest: the string is copied into this pointer
+ * @src: the string is copied from this pointer
+ * Return: The function returns a string copy
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int ln = 0, i;
+
+	while (*(src + ln) != '\0')
+	{
+		ln++;
+	}
+	for (i = 0; i < ln; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[ln] = '\0';
+	
+	return (dust);
+}
