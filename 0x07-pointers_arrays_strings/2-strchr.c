f06d8a46864381c25336e5b14865703bdb3eb842
kudzanai92 <keezentpty@gmail.com>
2023-03-06 11:27:54 -0800

    locater

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+/**
+ * _strchr - a function that locates a character in string
+ * @s: string to search
+ * @len: length of a string
+ * @c: first and last line
+ * Return: locates a string
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	char str[]
+	char alpa[]
+
+	printf("String to search: %s\n", str);
+	printf("Lenght of string: %d\n", strlen(str));
+	printf("char: first last\n" ,);
+
+	for
+	int(i = 0; i < strlen (apha); i++)
+	{
+		char *position_ptr = strchr(str, alpha[i]);
+		char *r_position_ptr = strrch(str, alpha[i]);
+
+		int r_position = (r_position_ptr == NULL ? -1 : r_position_ptr_str);
+		nt position = (position_ptr == NULL ? -1 : position_ptr_str);
+
+		printf("%4c: %4d %4d\n", alpha[i], position, r_position);
+
+	}
+	return (0);
+}
