d7f57049e45d7b45f330823dfb3b2288a28aa576
kudzanai92 <keezentpty@gmail.com>
2023-03-10 17:57:49 -0800

    whatsmyname

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - prints the name of the program
+ * main - prints the name of the programe
  * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: Always 0 (Success)
+ * @argv: array of argument
+ * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+
+int main(int argc_attribute_((unused)), char*argv[])
 {
 	printf("%s\n", *argv);
 
 	return (0);
 }
+ 
