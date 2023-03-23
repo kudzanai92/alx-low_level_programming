0df7284e1c1a017c22a72758e6ed212397d4179a
kudzanai92 <keezentpty@gmail.com>
2023-03-23 07:48:13 -0700

    function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
+/**
+ * main - a program that prints the opcodes of its own main function.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0;
+ */
 
-    int num_bytes = atoi(argv[1]);
-    if (num_bytes < 0) {
-        printf("Error\n");
-        return 2;
-    }
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	int num_bytes = atoi(argv[1]);
 
-    unsigned char *ptr = (unsigned char *)&main;
-    for (int i = 0; i < num_bytes; i++) {
-        printf("%02x", *(ptr + i));
-    }
-    printf("\n");
-
-    return 0;
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	unsigned char *ptr = (unsigned char *)&main;
+	int i
+	for (i = 0; i < 'num_bytes'; i++)
+	{
+		printf("%02x", *(ptr + i));
+	}
+	printf("\n");
+	return (0);
 }
-
