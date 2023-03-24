f6c41fd61df7f8d48b26494ef439abefbd1adafc
kudzanai92 <keezentpty@gmail.com>
2023-03-23 19:32:34 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,102 +1,50 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+
 
 /**
- * print_case_char - prints case for format of c
- * @arg: list of arguments
- *
- * Return: Nothing
- */
-void print_case_char(va_list *arg)
+* print_all - prints anything
+* @format: list of types of arguments passed to the function
+*/
+void print_all(const char * const format, ...)
 {
-	char c;
+int i = 0;
+char *str, *sep = "";
 
-	c = va_arg(*arg, int);
-	printf("%c", c);
+va_list list;
+
+va_start(list, format);
+
+if (format)
+{
+while (format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", sep, va_arg(list, int));
+break;
+case 'i':
+printf("%s%d", sep, va_arg(list, int));
+break;
+case 'f':
+printf("%s%f", sep, va_arg(list, double));
+break;
+case 's':
+str = va_arg(list, char *);
+if (!str)
+str = "(nil)";
+printf("%s%s", sep, str);
+break;
+default:
+i++;
+continue;
 }
-
-/**
- * print_case_int - prints case for format of i
- * @arg: list of arguments
- *
- * Return: Nothing
- */
-void print_case_int(va_list *arg)
-{
-	int num;
-
-	num = va_arg(*arg, int);
-	printf("%d", num);
+sep = ", ";
+i++;
 }
-
-/**
- * print_case_float - prints case for format of f
- * @arg: list of arguments
- *
- * Return: Nothing
- */
-void print_case_float(va_list *arg)
-{
-	double fl;
-
-	fl = va_arg(*arg, double);
-	printf("%f", fl);
 }
-
-/**
- * print_case_str - prints case for format of s
- * @arg: list of arguments
- *
- * Return: Nothing
- */
-void print_case_str(va_list *arg)
-{
-	char *temp;
-
-	temp = va_arg(*arg, char *);
-	if (temp == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", temp);
-}
-
-/**
- * print_all - prints anything provided to it
- * @format: list of types of args passed
- * @...: variable number of input arguments
- *
- * Descrition: prints all provided args based on the types in @format
- * Return: Nothing
- */
-void print_all(const char *const format, ...)
-{
-	int i = 0, j = 0;
-	va_list args;
-	char *separator = "";
-	print_case_t cases[] = {
-		{'c', print_case_char},
-		{'i', print_case_int},
-		{'f', print_case_float},
-		{'s', print_case_str},
-		{0, NULL},
-	};
-
-	va_start(args, format);
-	while (format[i] != '\0' && format != NULL)
-	{
-		while (cases[j].id != format[i] && cases[j].id)
-			j++;
-		if (j < 4)
-		{
-			printf("%s", separator);
-			cases[j].print_case(&args);
-			separator = ", ";
-		}
-		j = 0, i++;
-	}
-	printf("\n");
-	va_end(args);
+printf("\n");
+va_end(list);
 }
